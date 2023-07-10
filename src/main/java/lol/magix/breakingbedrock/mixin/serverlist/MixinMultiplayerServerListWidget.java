1c833757bc4e0a6018caa41097ecebe37d0fdc94
KingRainbow44 <kobedo11@gmail.com>
2023-07-10 13:00:36 -0400

    Fix being unable to ping Bedrock servers

--- src/main/java/lol/magix/breakingbedrock/mixin/serverlist/MixinMultiplayerServerListWidget.java
+++ src/main/java/lol/magix/breakingbedrock/mixin/serverlist/MixinMultiplayerServerListWidget.java
@@ -1,50 +1,34 @@
 package lol.magix.breakingbedrock.mixin.serverlist;
 
 import com.google.common.net.HostAndPort;
-import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.socket.nio.NioDatagramChannel;
-import lol.magix.breakingbedrock.BreakingBedrock;
-import lol.magix.breakingbedrock.objects.ThreadFactoryBuilder;
 import lol.magix.breakingbedrock.objects.absolute.NetworkConstants;
 import lol.magix.breakingbedrock.objects.game.BedrockServerInfo;
 import lol.magix.breakingbedrock.utils.NetworkUtils;
-import net.minecraft.GameVersion;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.multiplayer.MultiplayerServerListWidget;
 import net.minecraft.client.network.ServerInfo;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
-import net.minecraft.util.logging.UncaughtExceptionLogger;
-import org.cloudburstmc.netty.channel.raknet.RakChannelFactory;
-import org.cloudburstmc.netty.channel.raknet.config.RakChannelOption;
-import org.cloudburstmc.protocol.bedrock.BedrockClientSession;
-import org.cloudburstmc.protocol.bedrock.BedrockPeer;
 import org.cloudburstmc.protocol.bedrock.BedrockPong;
-import org.cloudburstmc.protocol.bedrock.netty.initializer.BedrockChannelInitializer;
-import org.cloudburstmc.protocol.bedrock.netty.initializer.BedrockClientInitializer;
-import org.cloudburstmc.protocol.bedrock.packet.BedrockPacketHandler;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.io.IOException;
-import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.ThreadPoolExecutor;
 
 @Mixin(MultiplayerServerListWidget.ServerEntry.class)
 public abstract class MixinMultiplayerServerListWidget {
-    private static final ThreadPoolExecutor SERVER_PING_POOL =
-            new ScheduledThreadPoolExecutor(5, ThreadFactoryBuilder.base());
+    @Unique
     private static final Text CANNOT_RESOLVE_TEXT = Text.translatable("multiplayer.status.cannot_resolve")
             .formatted(Formatting.DARK_RED);
+    @Unique
     private static final Text CANNOT_CONNECT_TEXT = Text.translatable("multiplayer.status.cannot_connect")
             .formatted(Formatting.DARK_RED);
 
@@ -59,7 +43,7 @@ public abstract class MixinMultiplayerServerListWidget {
         ci.cancel();
 
         var address = HostAndPort.fromString(this.server.address).withDefaultPort(19132);
-        SERVER_PING_POOL.submit(() -> {
+        new Thread(() -> {
             this.server.label = Text.translatable("multiplayer.status.pinging");
             this.server.ping = -1L;
             this.server.playerListSummary = null;
@@ -88,7 +72,7 @@ public abstract class MixinMultiplayerServerListWidget {
             this.server.protocolVersion = NetworkConstants.PACKET_CODEC.getProtocolVersion();
 
             MinecraftClient.getInstance().execute(this::saveFile);
-        });
+        }).start();
     }
 
     @Inject(method = "protocolVersionMatches", at = @At("HEAD"), cancellable = true)
