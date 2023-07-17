90130a9fa48d7bded991ecd9e3e24f49a9cc4efa
KingRainbow44 <kobedo11@gmail.com>
2023-07-17 14:58:31 -0400

    Handle LevelSoundEvent

---
+++ src/main/java/lol/magix/breakingbedrock/network/packets/bedrock/world/LevelSoundEventTranslator.java
@@ -0,0 +1,38 @@
+package lol.magix.breakingbedrock.network.packets.bedrock.world;
+
+import lol.magix.breakingbedrock.annotations.Translate;
+import lol.magix.breakingbedrock.network.translation.Translator;
+import lol.magix.breakingbedrock.objects.absolute.PacketType;
+import lol.magix.breakingbedrock.translators.blockstate.BlockStateTranslator;
+import lol.magix.breakingbedrock.utils.GameUtils;
+import net.minecraft.client.sound.PositionedSoundInstance;
+import net.minecraft.client.sound.SoundInstance;
+import net.minecraft.sound.SoundCategory;
+import org.cloudburstmc.protocol.bedrock.packet.LevelSoundEventPacket;
+
+@Translate(PacketType.BEDROCK)
+public final class LevelSoundEventTranslator extends Translator<LevelSoundEventPacket> {
+    @Override
+    public Class<LevelSoundEventPacket> getPacketClass() {
+        return LevelSoundEventPacket.class;
+    }
+
+    @Override
+    public void translate(LevelSoundEventPacket packet) {
+        var soundManager = this.client().getSoundManager();
+        var position = GameUtils.toBlockPos(packet.getPosition().toInt());
+
+        switch (packet.getSound()) {
+            case HIT -> {
+                var blockState = BlockStateTranslator.getRuntime2Java()
+                        .get(packet.getExtraData());
+                var soundGroup = blockState.getSoundGroup();
+
+                soundManager.play(new PositionedSoundInstance(soundGroup.getHitSound(), SoundCategory.BLOCKS,
+                        (soundGroup.getVolume() + 1.0F) / 8.0F,
+                        soundGroup.getPitch() * 0.5F,
+                        SoundInstance.createRandom(), position));
+            }
+        }
+    }
+}
