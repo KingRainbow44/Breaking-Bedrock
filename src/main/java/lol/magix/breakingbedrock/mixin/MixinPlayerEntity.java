6783ebea6b900b965e63c8704411e9eb22afd86e
KingRainbow44 <kobedo11@gmail.com>
2023-07-20 22:46:04 -0400

    Fix velocity and sprint

--- src/main/java/lol/magix/breakingbedrock/mixin/MixinPlayerEntity.java
+++ src/main/java/lol/magix/breakingbedrock/mixin/MixinPlayerEntity.java
@@ -1,10 +1,12 @@
 package lol.magix.breakingbedrock.mixin;
 
 import lol.magix.breakingbedrock.network.BedrockNetworkClient;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerEntity.class)
@@ -13,5 +15,23 @@ public final class MixinPlayerEntity {
     public void getAttackCooldownProgressPerTick(CallbackInfoReturnable<Float> cir) {
         if (!BedrockNetworkClient.connected()) return;
         cir.setReturnValue((float) (1.0 / 20000));
+    }
+
+    @Inject(method = "attack", at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/entity/player/PlayerEntity;setSprinting(Z)V"
+    ), cancellable = true)
+    public void removeSprint(Entity target, CallbackInfo ci) {
+        if (!BedrockNetworkClient.connected()) return;
+        ci.cancel();
+    }
+
+    @Inject(method = "attack", at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/entity/player/PlayerEntity;setVelocity(Lnet/minecraft/util/math/Vec3d;)V"
+    ), cancellable = true)
+    public void removeVelocity(Entity target, CallbackInfo ci) {
+        if (!BedrockNetworkClient.connected()) return;
+        ci.cancel();
     }
 }
