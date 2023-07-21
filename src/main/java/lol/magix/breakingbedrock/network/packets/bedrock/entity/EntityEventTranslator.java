b28e4e59f55211e38c5b6806dba6af7c7f1143a5
KingRainbow44 <kobedo11@gmail.com>
2023-07-20 22:04:45 -0400

    Handle entity deaths

--- src/main/java/lol/magix/breakingbedrock/network/packets/bedrock/entity/EntityEventTranslator.java
+++ src/main/java/lol/magix/breakingbedrock/network/packets/bedrock/entity/EntityEventTranslator.java
@@ -3,8 +3,10 @@ package lol.magix.breakingbedrock.network.packets.bedrock.entity;
 import lol.magix.breakingbedrock.annotations.Translate;
 import lol.magix.breakingbedrock.network.translation.Translator;
 import lol.magix.breakingbedrock.objects.absolute.PacketType;
+import net.minecraft.entity.EntityStatuses;
 import net.minecraft.network.packet.s2c.play.EntityAnimationS2CPacket;
 import net.minecraft.network.packet.s2c.play.EntityDamageS2CPacket;
+import net.minecraft.network.packet.s2c.play.EntityStatusS2CPacket;
 import org.cloudburstmc.protocol.bedrock.packet.EntityEventPacket;
 
 @Translate(PacketType.BEDROCK)
@@ -28,6 +30,8 @@ public final class EntityEventTranslator extends Translator<EntityEventPacket> {
                     new EntityAnimationS2CPacket(entity, EntityAnimationS2CPacket.SWING_MAIN_HAND));
             case HURT -> this.javaClient().processPacket(
                     new EntityDamageS2CPacket(entity, world.getDamageSources().generic()));
+            case DEATH -> this.javaClient().processPacket(
+                    new EntityStatusS2CPacket(entity, EntityStatuses.ADD_DEATH_PARTICLES));
         }
     }
 }
