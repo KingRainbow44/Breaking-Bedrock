8b392d6593588dfc18ffd41672c8764ffa32f51f
KingRainbow44 <kobedo11@gmail.com>
2023-07-27 18:26:57 -0400

    Allow for uberjar building

--- src/main/java/lol/magix/breakingbedrock/mixin/protocol/MixinStartGameSerializer_v428.java
+++ src/main/java/lol/magix/breakingbedrock/mixin/protocol/MixinStartGameSerializer_v428.java
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(StartGameSerializer_v428.class)
 public final class MixinStartGameSerializer_v428 extends StartGameSerializer_v419 {
-    @Inject(method = "readSyncedPlayerMovementSettings", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "readSyncedPlayerMovementSettings", at = @At("HEAD"), cancellable = true, remap = false)
     public void readSyncedPlayerMovementSettings(ByteBuf buffer, StartGamePacket packet, CallbackInfo ci) {
         ci.cancel(); // Purely handle the packet here.
 
